f06c1652eed3fad321a61fb8d80fa89444dae1e7
Germain kataku <katakugermain@gmail.com>
2024-11-16 08:20:48 +0100

    remove url cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     origin: isProduction ? 'https://wmrh-rdc.up.railway.app' : 'http://localhost:4200',
-    // origin: isProduction ? 'https://wmrh.onrender.com' : 'http://localhost:4200',
     credentials: true
   });
   const PORT = process.env.PORT || 3001;
